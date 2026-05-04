59032d473ddd1d02297913832e1140a2a0776bcd
Daniel Sims <benjbanh@gmail.com>
2026-05-03 17:52:24 -0700

    Same as before, SLAM verifiably working. Added fresh multi_robot_bringup before git revert

---
+++ src/multi_robot_bringup/setup.py
@@ -0,0 +1,40 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'multi_robot_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+            glob(os.path.join('launch', '*.py'))),
+        (os.path.join('share', package_name, 'config'),
+            glob(os.path.join('config', '*.yaml')) +
+            glob(os.path.join('config', '*.xml')) +
+            glob(os.path.join('config', '*.sh'))),
+        (os.path.join('share', package_name, 'config', 'maps'),
+            glob(os.path.join('config', 'maps', '*'))),
+        (os.path.join('share', package_name, 'rviz'),
+            glob(os.path.join('rviz', '*.rviz'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='agent1',
+    maintainer_email='benjbanh@gmail.com',
+    description='Multi-robot bringup: AMCL localization with shared map frame',
+    license='Apache-2.0',
+    extras_require={'test': ['pytest']},
+    entry_points={
+        'console_scripts': [
+            'pose_aggregator = multi_robot_bringup.pose_aggregator:main',
+            'robot_pose_broadcaster = multi_robot_bringup.robot_pose_broadcaster:main',
+            'pose_normalizer = multi_robot_bringup.pose_normalizer:main',
+        ],
+    },
+)
